0d060729ca1c2252a650c762fcf967e09500d517
elagamy257 <elagamy257@gmail.com>
2025-08-19 01:52:20 +0300

    type annotations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,64 @@
-console.log(Math.floor(10.15))
+// let name: string = "elagamy"
+// let age: number = 20
+// let hire: boolean = true
+// let all: any 
+
+// all = 10
+// all = "hello"
+// all = true
+
+// arr
+
+// let arr: string[] = ["hello", "world"]
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]?.repeat(2));
+// }
+
+// multi arr
+
+// let arrNum:  number[] = [ 10,  20, 30, 40, 50]
+// let arrStr: string [] = ["hello", "world", "typescript" ]
+// let multi: (string | number)[] = ["hello", 10, "world", 20]
+// let multi1: (string | number | string [])[] = ["hello", 10, "world", ["typescript", "javascript"]]
+// let multi2: (string | number)[][] = [["hello", 10], ["world", 20], ["typescript", 30]]  
+
+// func
+
+// let show= true
+// function showName(name: string, age: number): string {
+//     if (show) {
+//       return `My name is ${name} and I am ${age} years old.`
+//     }
+
+//     return `Information is hidden.`
+// }
+
+// console.log(showName('aya',21))
+
+// function showName(name: string, age: number, country?: string): string {
+//   return ` ${name} ${age} `
+// }
+// console.log(showName('aya', 21)) // Output: aya 21
+
+// func rest
+
+// function addNumbers(...numbers: number[]) {
+//     let res=0;
+    // for (let i = 0; i < numbers.length; i++) {
+    //     res += numbers[i];
+    // }
+
+    // for (const num of numbers) {
+    //     res += num;
+    // }
+//     numbers.forEach(num => {
+//         res += num;
+//     });
+
+//     return res;
+// }
+// console.log(addNumbers(1, 2, 3, 4, 5, 10.6, +true)); 
+
+
+
+
--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,5 @@
-console.log(Math.round(10.15))
+function add(x: number, y: number): number {
+    return x + y;
+}
+console.log(add(5, 10)); // Output: 15
+console.log(typeof add(5, 10)); // Output: 15
