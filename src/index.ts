fcd8f5e59edbf21657a310c64469871a94f8ea26
elagamy257 <elagamy257@gmail.com>
2025-08-19 19:26:30 +0300

    arro func

--- src/index.ts
+++ src/index.ts
@@ -60,5 +60,14 @@
 // console.log(addNumbers(1, 2, 3, 4, 5, 10.6, +true)); 
 
 
+// - Anonymous Function
 
+
+const add = function(num1: number, num2: number) : number {
+    return num1 + num2;
+}
+console.log(add(10, 20));
+
+const addWithArrow = (num1: number, num2: number) : number => num1 + num2;
+console.log(addWithArrow(10, 20));
 
